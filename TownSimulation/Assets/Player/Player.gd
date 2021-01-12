96fe03256c6b55d2b3b8fdfba4d137946e6546b4
jameslocke93 <james_locke93@hotmail.co.uk>
2021-01-12 17:55:07 +0000

    20210112

--- TownSimulation/Assets/Player/Player.gd
+++ TownSimulation/Assets/Player/Player.gd
@@ -1,18 +1,27 @@
-extends KinematicBody2D
+extends Node2D
 
 
 onready var movementGrid = $MovementGrid
+onready var buildButton = $MovementGrid/BuildButton
+
+
+func _ready():
+	buildButton.rect_position = Vector2(700, 600)
+	buildButton.visible = false
 
 
 func _physics_process(delta):
 	if Input.is_mouse_button_pressed(1):	# Checks if left mouse butten is pressed
 		var mouse_to_map_position = movementGrid.world_to_map(get_global_mouse_position())
-		var destination = movementGrid.map_to_world(mouse_to_map_position)
-		position = destination
-		check_world_tile(mouse_to_map_position)
+		if check_world_tile(mouse_to_map_position):
+			position = movementGrid.map_to_world(mouse_to_map_position)
 
 
 func check_world_tile(destination):
 	var world = get_tree().current_scene
 	var world_map = world.find_node("Map")
-	print(world_map.get_tile(destination))
+	if world_map.get_tile(destination) >= 0:
+		buildButton.visible = true
+		return true
+	return false
+	
